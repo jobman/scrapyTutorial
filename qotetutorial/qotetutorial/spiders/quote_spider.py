7fd46a375a257f64a2bafba2f4baf107e57ac067
Marina <ekvin.m@gmail.com>
2020-04-28 15:11:36 +0300

    all

---
+++ qotetutorial/qotetutorial/spiders/quote_spider.py
@@ -0,0 +1,12 @@
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+    name = 'quotes'
+    start_urls = [
+        'http://quotes.toscrape.com/'
+    ]
+
+    def parse(self, response):
+        title = response.css('title::text').extract()
+        yield {'titleText': title}
