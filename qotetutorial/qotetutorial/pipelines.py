25f2b69d96ba95b98dce89f1ff1ea54b709db5fe
jobman <jobmanalex@gmail.com>
2020-05-03 18:57:14 +0300

    mongo

--- qotetutorial/qotetutorial/pipelines.py
+++ qotetutorial/qotetutorial/pipelines.py
@@ -5,20 +5,24 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
-import sqlite3
+import pymongo
 
 
 class QotetutorialPipeline:
 
     def __init__(self):
-        self.create_connection()
-        self.create_table()
+        self.conn = pymongo.MongoClient(
+            'localhost',
+            27017
+        )
+        self.db = self.conn['quotes_db']
+        self.collection = self.db['quotes_tb']
 
-    def create_connection(self):
+    def create_connection_sql(self):
         self.conn = sqlite3.connect("quotes.db")
         self.curr = self.conn.cursor()
 
-    def create_table(self):
+    def create_table_sql(self):
         self.curr.execute("""drop table if exists quotes""")
         self.curr.execute("""create table quotes(
                         title text,
@@ -27,10 +31,10 @@ class QotetutorialPipeline:
                         )""")
 
     def process_item(self, item, spider):
-        self.store_in_db(item)
+        self.collection.insert(dict(item))
         return item
 
-    def store_in_db(self, item):
+    def store_in_db_sql(self, item):
         self.curr.execute("""insert into quotes values (?,?,?)""", (
             item['title'][0],
             item['author'][0],
